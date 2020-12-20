fcb76a448c2fd1547b45e0e2d818bd56362df885
Benjamin Rose <benjaminharringtonrose@gmail.com>
2020-12-20 18:42:48 -0500

    change to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-var execSh = require("../");
-console.log("here")
+var execSh = require("./");
+console.log("here");
 // run interactive bash shell
-() => {
-  execSh("./release.sh", { cwd: "/src" }, function (err) {
-    if (err) {
-      console.log("Exit code: ", err.code);
-      return;
-    }
-  });
-};
+
+execSh("./release.sh", { cwd: "/src" }, function (err) {
+  if (err) {
+    console.log("Exit code: ", err.code);
+    return;
+  }
+});
