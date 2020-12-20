decca6874941d8a47c62db06cef98628a3282ff0
Benjamin Rose <benjaminharringtonrose@gmail.com>
2020-12-20 18:56:03 -0500

    fix execSh function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 var execSh = require("exec-sh");
-console.log("here");
-// run interactive bash shell
 
-execSh("./release.sh", { cwd: "/src" }, function (err) {
-  if (err) {
-    console.log("Exit code: ", err.code);
-    return;
-  }
-});
+// run interactive bash shell
+execSh("./release.sh");
