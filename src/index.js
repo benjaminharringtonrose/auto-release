ab8100d24d223985997c50534d2b5efc081578c2
Benjamin Rose <benjaminharringtonrose@gmail.com>
2020-12-20 17:53:46 -0500

    initialized npm

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var execSh = require("../");
+
+// run interactive bash shell
+() => {
+  execSh("./release.sh", { cwd: "/src" }, function (err) {
+    if (err) {
+      console.log("Exit code: ", err.code);
+      return;
+    }
+  });
+};
