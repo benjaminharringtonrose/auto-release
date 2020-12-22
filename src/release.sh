cc2d7dbe705bdf5957437e4fc0027c33ec4aedfe
Benjamin Rose <benjaminharringtonrose@gmail.com>
2020-12-22 16:20:00 -0500

    making some changes

--- src/release.sh
+++ src/release.sh
@@ -15,10 +15,9 @@
 # 12. merge develop branch
 # 13. push commit release branch to remote origin
 
-BLUE_TEXT='\033[0;34m'
-RED_TEXT='\033[0;31m'
-PURPLE_TEXT='\033[0;35m'
-NO_COLOR='\033[0m'
+blueText='\033[0;34m'
+redText='\033[0;31m'
+purpleText='\033[0;35m'
 
 # current Git branch
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
@@ -35,68 +34,56 @@ git checkout $developBranch
 # master branch validation
 if [ $branch = "develop" ]; then
 
-	printf ${PURPLE_TEXT}"\nHave you already incremented the package.json? y/n ${NO_TEXT}"
-	read userResponse
+	npm version bump
 
-	if [ $userResponse == "y" ]; then
+	packageVersion=$(cat package.json \
+		| grep version \
+		| head -1 \
+		| awk -F: '{ print $2 }' \
+		| sed 's/[",]//g' \
+		| tr -d '[:space:]')
 
-		# Version value
-		PACKAGE_VERSION=$(cat package.json \
-			| grep version \
-			| head -1 \
-			| awk -F: '{ print $2 }' \
-			| sed 's/[",]//g' \
-			| tr -d '[:space:]')
+	
 
-		printf ${PURPLE_TEXT}"\nRelease version $PACKAGE_VERSION${NO_TEXT}\n\n"
+	printf "\nRelease version $packageVersion\n\n"
 
-		# 1.0.0, 1.0.1, 1.1.0, etc
-		versionLabel=$PACKAGE_VERSION
+	# 1.0.0, 1.0.1, 1.1.0, etc
+	versionLabel=$packageVersion
 
-		# establish branch and tag name variables
-		releaseBranch="release/$PACKAGE_VERSION"
-		tagName="open-release-v$PACKAGE_VERSION"
+	# establish branch and tag name variables
+	releaseBranch="release/$packageVersion"
+	tagName="open-release-v$packageVersion"
 
-		printf "\n${BLUE_TEXT}Started releasing $PACKAGE_VERSION for $projectName -->${NO_COLOR}\n\n"
+	printf "\n${blueText}Started releasing $packageVersion for $projectName -->\n\n"
 
-		# pull the latest version of the code from develop and create empty commit from develop branch
-		printf "\n${BLUE_TEXT}git pull && git commit --allow-empty -m${NO_COLOR}\n\n"
-		git pull && git commit --allow-empty -m "Creating branch $releaseBranch"
+	# pull the latest version of the code from develop and create empty commit from develop branch
+	printf "\ngit pull && git commit --allow-empty -m\n\n"
+	git pull && git commit --allow-empty -m "Creating branch $releaseBranch"
 
-		# create tag for new version from develop and push commit to remote origin
-		printf "\n${BLUE_TEXT}git tag $tagName && git push${NO_COLOR}\n\n"
-		git tag $tagName && git push
+	# create tag for new version from develop and push commit to remote origin
+	printf "\ngit tag $tagName && git push\n\n"
+	git tag $tagName && git push
 
-		# push tag to remote origin
-		printf "\n${BLUE_TEXT}git push --tags origin${NO_COLOR}\n\n"
-		git push --tags origin 
-		
-		# create the release branch from the develop branch
-		printf "\n${BLUE_TEXT}git checkout -b $releaseBranch $developBranch ${NO_COLOR}\n\n"
+	# push tag to remote origin
+	printf "\ngit push --tags origin\n\n"
+	git push --tags origin 
+	
+	# create the release branch from the develop branch
+	printf "\ngit checkout -b $releaseBranch $developBranch\n\n"
 
-		git checkout -b $releaseBranch $developBranch
+	git checkout -b $releaseBranch $developBranch
 
-		# merge develop branch
-		printf "\n${BLUE_TEXT}git merge $developBranch ${NO_COLOR}\n\n"		
-		git merge $developBranch
+	# merge develop branch
+	printf "\ngit merge $developBranch\n\n"		
+	git merge $developBranch
 
-		# push local releaseBranch to remote
-		git push -u origin $releaseBranch
+	# push local releaseBranch to remote
+	git push -u origin $releaseBranch
 
-		printf "\n${PURPLE_TEXT}$PACKAGE_VERSION is successfully created for $projectName!${NO_COLOR}\n\n"
-
-	elif [ $userResponse == "n" ]; then
-
-		printf "\n${RED_TEXT}Error: Please increment package${NO_COLOR}\n\n"
-
-	else
-
-		printf "\n${RED_TEXT}Error: Incorrect response${NO_COLOR}\n\n"
-
-	fi
+	printf "\n${purpleText}$packageVersion is successfully created for $projectName!\n\n"
 
 else 
 
-	printf "${RED_TEXT}Please make sure you are on develop branch!${NO_COLOR}\n"
+	printf "${redText}Please make sure you are on develop branch!\n"
 
 fi
