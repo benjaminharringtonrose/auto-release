6a73d27327fa9ccfb194e8ba0341b62b38612bea
Benjamin Rose <benjaminharringtonrose@gmail.com>
2020-12-28 17:31:56 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const shell = require("shelljs");
 
 async function sequentialExec() {
   try {
-    await shell.exec("npm version patch");
+    await shell.exec("yarn version");
     await shell.exec("./src/release.sh");
   } catch (e) {
     console.log(e);
